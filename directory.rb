834f00101fbbbbd7ab677d4464ab784320de026f
Tam Borine <tamborinehands@gmail.com>
2016-08-02 15:17:46 +0000

    added favourite food to students hash arr and made all items inputable by user

--- directory.rb
+++ directory.rb
@@ -14,18 +14,25 @@
 =end
 
 def input_students
-puts "Please enter the names of the students"
-puts "To finish, just hit return twice"
-students = []
-name = gets.chomp
-
-while !name.empty? do
-	students << {name: name, cohort: :november}
-	puts "Now we have #{students.count} students"
-	
-	name = gets.chomp
-end
-students
+  students = []
+while true  do
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty arra
+ 
+  # get the first name
+  name = gets.chomp
+  puts "Please enter the cohort of this student"
+  cohort = gets.chomp
+  puts "Please enter their favourite food"
+  food = gets.chomp
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+   if name.length == 0 ; break ; end
+  end
+  # return the array of students
+  students
 end
 
 def print_header
@@ -34,7 +41,12 @@ puts "-------------"
 end
 
 def print (students)
-students.each_with_index do |student,index| puts "#{index + 1}. #{student[:name]} #{student[:cohort]} cohort " end
+index = 0
+while index < students.length
+
+ puts "#{index + 1}. #{students[index][:name]} #{students[index][:cohort]} cohort "
+index += 1 
+end
 end
 
 def print_if_under12(students)
